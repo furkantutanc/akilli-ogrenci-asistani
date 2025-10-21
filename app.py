c96163d7463389aec470b4353dbb464e2057c9d8
Furkan Tutanç <furkantutanc@gmail.com>
2025-10-22 01:56:38 +0300

    Fix: RAG

--- app.py
+++ app.py
@@ -613,7 +613,7 @@ class RAGIsleyici:
                 
                 
                 self.embeddings = HuggingFaceEmbeddings(
-    model_name="hkunlp/instructor-base",
+    model_name="sentence-transformers/all-MiniLM-L6-v2",
     model_kwargs={'device': 'cpu'}
 )
                 
